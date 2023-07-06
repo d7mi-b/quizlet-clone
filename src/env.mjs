7a2ec6c38524188115a65a07acffa7a381c8be7d
Abdulrahman Bahyan <d7mii.b@gmail.com>
2023-07-06 09:49:19 +0300

    try to fix error of invalid emvaermont variable

--- src/env.mjs
+++ src/env.mjs
@@ -7,7 +7,7 @@ export const env = createEnv({
    * isn't built with invalid env vars.
    */
   server: {
-    DATABASE_URL: z.string().url(),
+    DATABASE_URL: z.string(),
     NODE_ENV: z.enum(["development", "test", "production"]),
     JWT_SECRET_KEY: z.string(),
     UPLOADTHING_APP_ID: z.string(),
